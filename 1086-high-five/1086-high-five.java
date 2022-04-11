a75874b91cd6123e062a1966fcdd1ea163cf1cb1
Kaiwei L <kaiweiluo5@gmail.com>
2022-04-11 11:50:26 -0400

    Time: 7 ms (52.62%), Space: 47 MB (11.76%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,35 +1,26 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        HashMap<Integer, List<Integer>> counter = new HashMap<>();
+        HashMap<Integer, ArrayList<Integer>> dict = new HashMap<>();
         for (int i = 0; i < items.length; i++) {
-            int id = items[i][0];
-            int score = items[i][1];
-            List<Integer> scores = counter.getOrDefault(id, new ArrayList<Integer>());
+            Integer id = items[i][0];
+            Integer score = items[i][1];
+            ArrayList<Integer> scores = dict.getOrDefault(id, new ArrayList<Integer>());
             scores.add(score);
-            counter.put(id, scores);
+            dict.put(id, scores);
         }
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        for (int id : counter.keySet()) {
-            List<Integer> scoresList = counter.get(id);
-            scoresList.sort((a, b) -> b - a);
-            int topScoreSum = 0;
+        int[][] ans = new int[dict.size()][2];
+        int indx = 0;
+        for (Integer id : dict.keySet()) {
+            ArrayList<Integer> scores = dict.get(id);
+            scores.sort((Integer a, Integer b) -> b - a);
+            int sum = 0;
             for (int i = 0; i < 5; i++) {
-                topScoreSum += scoresList.get(i);
+                sum += scores.get(i);
             }
-            int avgScore = topScoreSum / 5;
-            List<Integer> student = new ArrayList<Integer>();
-            student.add(id);
-            student.add(avgScore);
-            ans.add(student);
+            ans[indx][0] = id;
+            ans[indx][1] = sum / 5;
+            indx++;
         }
-        ans.sort((a, b) -> a.get(0) - b.get(0));
-        int[][] res = new int[ans.size()][2];
-        for (int i = 0; i < ans.size(); i++) {
-            List<Integer> s = ans.get(i);
-            res[i][0] = s.get(0);
-            res[i][1] = s.get(1);
-        }
-        return res;
-        
+        return ans;
     }
 }
