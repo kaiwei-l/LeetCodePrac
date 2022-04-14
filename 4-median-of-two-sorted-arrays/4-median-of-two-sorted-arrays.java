424f2a1c1eb5c8eeb39710f8bb9fe9cad078e872
Kaiwei L <kaiweiluo5@gmail.com>
2022-04-14 17:04:32 -0400

    Time: 2 ms (91.86%), Space: 43.2 MB (89.72%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,23 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+        
         int x = nums1.length;
         int y = nums2.length;
         
         int low = 0;
-        int high = nums1.length;
-        
+        int high = x;
         while (low <= high) {
             int partitionX = (low + high) / 2;
             int partitionY = (x + y + 1) / 2 - partitionX;
             
-            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
-            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+            int maxLeftX;
+            int minRightX;
+            int maxLeftY;
+            int minRightY;
             
-            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
-            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+            if (partitionX == 0) {
+                maxLeftX = Integer.MIN_VALUE;
+            } else {
+                maxLeftX = nums1[partitionX - 1];
+            }
+            if (partitionX == x) {
+                minRightX = Integer.MAX_VALUE;
+            } else {
+                minRightX = nums1[partitionX];
+            }
+            if (partitionY == 0) {
+                maxLeftY = Integer.MIN_VALUE;
+            } else {
+                maxLeftY = nums2[partitionY - 1];
+            }
+            if (partitionY == y) {
+                minRightY = Integer.MAX_VALUE;
+            } else {
+                minRightY = nums2[partitionY];
+            }
             
             if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
                 if ((x + y) % 2 == 0) {
@@ -31,6 +49,6 @@ class Solution {
                 low = partitionX + 1;
             }
         }
-        return -1;
+        return 0.0;
     }
 }
