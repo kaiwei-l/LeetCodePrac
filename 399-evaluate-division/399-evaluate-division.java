a1279735815bcbbb2097fe960637e99f27399c2e
Kaiwei L <kaiweiluo5@gmail.com>
2022-04-25 23:47:34 -0400

    Time: 2 ms (55.92%), Space: 42.5 MB (63.14%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,66 +1,58 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String, HashMap<String, Double>> adjList = new HashMap<>();
-        
+        HashMap<String, ArrayList<Pair<String, Double>>> adjList = new HashMap<String, ArrayList<Pair<String, Double>>>();
         for (int i = 0; i < equations.size(); i++) {
             List<String> equation = equations.get(i);
-            String a = equation.get(0);
-            String b = equation.get(1);
             Double value = values[i];
-            Double revValue = 1/value;
-            HashMap<String, Double> edgesOfA = adjList.getOrDefault(a, new HashMap<String, Double>());
-            edgesOfA.put(b, value);
-            adjList.put(a, edgesOfA);
-            HashMap<String, Double> edgesOfB = adjList.getOrDefault(b, new HashMap<String, Double>());
-            edgesOfB.put(a, revValue);
-            adjList.put(b, edgesOfB);
+            String var1 = equation.get(0);
+            String var2 = equation.get(1);
+            ArrayList<Pair<String, Double>> edges1 = adjList.getOrDefault(var1, new ArrayList<Pair<String, Double>>());
+            ArrayList<Pair<String, Double>> edges2 = adjList.getOrDefault(var2, new ArrayList<Pair<String, Double>>());
+            edges1.add(new Pair(var2, value));
+            edges2.add(new Pair(var1, 1 / value));
+            adjList.put(var1, edges1);
+            adjList.put(var2, edges2);
         }
         
         double[] ans = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
             List<String> q = queries.get(i);
-            String a = q.get(0);
-            String b = q.get(1);
-            if (!adjList.containsKey(a) || !adjList.containsKey(b)) {
-                ans[i] = -1.0;
-            } else if (a.equals(b)) {
-                ans[i] = 1.0;
-            } else {
+            String var1 = q.get(0);
+            String var2 = q.get(1);
+            if (adjList.containsKey(var1)) {
                 HashMap<String, Boolean> visited = new HashMap<>();
+                visited.put(var1, true);
                 ArrayDeque<String> stack = new ArrayDeque<>();
-                stack.addLast(a);
-                visited.put(a, true);
-                Pair<Boolean, Double> p = dfs(adjList, visited, b, stack);
-                ans[i] = p.getValue();
+                stack.add(var1);
+                Double res = dfs(adjList, stack, visited, var2);
+                ans[i] = res;
+            } else {
+                ans[i] = -1.0;
             }
         }
-        
         return ans;
-        
     }
     
-    public Pair<Boolean, Double> dfs(HashMap<String, HashMap<String, Double>> adjList, HashMap<String, Boolean> visited, String target, ArrayDeque<String> stack) {
+    public double dfs(HashMap<String, ArrayList<Pair<String, Double>>> adjList, ArrayDeque<String> stack, HashMap<String, Boolean> visited, String goal) {
         while (!stack.isEmpty()) {
-            String var = stack.removeLast();
-            HashMap<String, Double> adjEdges = adjList.get(var);
-            for (String node : adjEdges.keySet()) {
-                if (!visited.containsKey(node)) {
-                    visited.put(node, true);
-                    if (node.equals(target)) {
-                        Pair<Boolean, Double> p = new Pair<>(true, adjEdges.get(node));
-                        return p;
-                    } else {
-                        stack.addLast(node);
-                        Pair<Boolean, Double> p = dfs(adjList, visited, target, stack);
-                        if (p.getKey()) {
-                            Double tempVal = p.getValue();
-                            tempVal *= adjEdges.get(node);
-                            return new Pair<Boolean, Double>(true, tempVal);
+            String node = stack.removeLast();
+            ArrayList<Pair<String, Double>> edges = adjList.get(node);
+            for (Pair<String, Double> p : edges) {
+                String nextNode = p.getKey();
+                if (nextNode.equals(goal)) {
+                    return p.getValue();
+                } else {
+                    if (!visited.containsKey(nextNode) || !visited.get(nextNode)) {
+                        visited.put(nextNode, true);
+                        stack.addLast(nextNode);
+                        double ans = dfs(adjList, stack, visited, goal);
+                        if (ans != -1.0) {
+                            return ans * p.getValue();
                         }
-                    }
+                    }   
                 }
             }
         }
-        return new Pair<Boolean, Double>(false, -1.0);
+        return -1;
     }
 }
