fc292ddd3a841665c78b55b69441923b1f179a45
Kaiwei L <kaiweiluo5@gmail.com>
2022-04-26 16:41:00 -0400

    Time: 1 ms (82.69%), Space: 43.5 MB (65.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,69 +1,31 @@
 class Solution {
-    public void swap(int left, int right, int[] nums) {
-        int temp = nums[right];
-        nums[right] = nums[left];
-        nums[left] = temp;
-    }
-    
-    public void reverse(int left, int right, int[] nums) {
-        while (left <= right) {
-            swap(left, right, nums);
-            left++;
-            right--;
-        }
+    public void exchange(int left, int right, int[] nums) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
     
     public void nextPermutation(int[] nums) {
-        if (nums.length == 1) {
-            return;
-        } else if (nums.length == 2) {
-            swap(0, 1, nums);
-            return;
-        }
-        
-        boolean isRev = true;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] < nums[i + 1]) {
-                isRev = false;
-                break;
-            }
-        }
-        if (isRev) {
-            reverse(0, nums.length - 1, nums);
-            return;
-        }
-        
-        int right = nums.length - 1;
-        int left = nums.length - 2;
-        while (left != -1) {
-            if (nums[left] < nums[right]) {
-                int rightMin = nums.length - 1;
-                while (rightMin != right) {
-                    if (nums[rightMin] > nums[left]) {
-                        break;
-                    } else {
-                        rightMin--;
+        int midPoint = -1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                int swapPoint = i;
+                for (int indx = i; indx < nums.length; indx++) {
+                    if (nums[i - 1] < nums[indx]) {
+                        swapPoint = indx;
                     }
                 }
-                swap(left, rightMin, nums);
-                
-                // int curr = rightMin;
-                // int currNext = rightMin + 1;
-                // while (currNext != nums.length) {
-                //     if (nums[curr] < nums[currNext]) {
-                //         swap(curr, currNext, nums);
-                //         curr++;
-                //         currNext++;
-                //     } else {
-                //         break;
-                //     }
-                // }
-                reverse(right, nums.length - 1, nums);
+                exchange(i - 1, swapPoint, nums);
+                midPoint = i;
                 break;
-            } else {
-                left--;
-                right--;
             }
+        }
+        int left = (midPoint == -1) ? 0 : midPoint;
+        int right = nums.length - 1;
+        while (left < right) {
+            exchange(left, right, nums);
+            left++;
+            right--;
         }
     }
 }
