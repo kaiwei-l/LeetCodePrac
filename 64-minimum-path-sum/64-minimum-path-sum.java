2393a09dbe045895e7cf58a72bd411bbbb3a7f1c
Kaiwei L <kaiweiluo5@gmail.com>
2022-04-27 22:30:49 -0400

    Time: 1 ms (99.60%), Space: 44.9 MB (89.05%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,20 +1,37 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int rowLimit = grid.length;
-        int colLimit = grid[0].length;
-        int[][] dp = new int[rowLimit][colLimit];
-        dp[rowLimit - 1][colLimit - 1] = grid[rowLimit - 1][colLimit - 1];
-        for (int row = rowLimit - 1; row > -1; row--) {
-            for (int col = colLimit - 1; col > -1; col--) {
-                if (row == rowLimit - 1 && col != colLimit - 1) {
-                    dp[row][col] = grid[row][col] + dp[row][col + 1];
-                } else if (row != rowLimit - 1 && col == colLimit - 1) {
-                    dp[row][col] = grid[row][col] + dp[row + 1][col];
-                } else if (row != rowLimit - 1 && col != colLimit - 1) {
-                    dp[row][col] = grid[row][col] + Math.min(dp[row + 1][col], dp[row][col + 1]);
-                }
-            }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        dp[0][0] = grid[0][0];
+        visited[0][0] = true;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+            visited[i][0] = true;
         }
-        return dp[0][0];
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = grid[0][j] + dp[0][j - 1];
+            visited[0][j] = true;
+        }
+        return backtrack(dp, visited, grid, m - 1, n - 1);
+    }
+    
+    private int backtrack(int[][] dp, boolean[][] visited, int[][] grid, int row, int col) {
+        if (visited[row][col]) {
+            return dp[row][col];
+        } else {
+            int newRow = row - 1;
+            int newCol = col - 1;
+            visited[row][col] = true;
+            if (newRow >= 0 && newCol >= 0) {
+                dp[row][col] = Math.min(backtrack(dp, visited, grid, row, newCol), backtrack(dp, visited, grid, newRow, col)) + grid[row][col];   
+            } else if (newRow < 0) {
+                dp[row][col] = backtrack(dp, visited, grid, row, newCol) + grid[row][col];
+            } else {
+                dp[row][col] = backtrack(dp, visited, grid, newRow, col) + grid[row][col];
+            }
+            return dp[row][col];
+        }
     }
 }
