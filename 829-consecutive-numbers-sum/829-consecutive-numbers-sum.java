1f18f6ec8ffd7211525d187d2a91df20dc523355
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-02 15:40:37 -0400

    Time: 3 ms (93.57%), Space: 38.7 MB (98.42%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
@@ -1,9 +1,10 @@
 class Solution {
     public int consecutiveNumbersSum(int n) {
         int count = 0;
-        int upperLimit = (int)(Math.sqrt(2*n + 0.25) - 0.5);
+        int upperLimit = (int)(Math.sqrt(2 * n + 0.25) - 0.5);
         for (int k = 1; k <= upperLimit; k++) {
-            if ((n - k * (k + 1) / 2) % k == 0) count++;
+            n -= k;
+            if (n % k == 0) count++;
         }
         return count;
     }
