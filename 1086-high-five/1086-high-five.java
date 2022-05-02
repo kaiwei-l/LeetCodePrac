df96cd5ba99980adefe73c32e18de1c052a31ff3
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-02 14:54:04 -0400

    Time: 8 ms (39.82%), Space: 47 MB (12.43%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,26 +1,30 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        HashMap<Integer, ArrayList<Integer>> dict = new HashMap<>();
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
         for (int i = 0; i < items.length; i++) {
-            Integer id = items[i][0];
-            Integer score = items[i][1];
-            ArrayList<Integer> scores = dict.getOrDefault(id, new ArrayList<Integer>());
-            scores.add(score);
-            dict.put(id, scores);
+            int[] tuple = items[i];
+            int id = tuple[0];
+            int score = tuple[1];
+            PriorityQueue<Integer> q = map.getOrDefault(id, new PriorityQueue<Integer>((a, b) -> a - b));
+            q.add(score);
+            if (q.size() > 5) {
+                q.poll();
+            }
+            map.put(id, q);
         }
-        int[][] ans = new int[dict.size()][2];
+        int[][] ans = new int[map.size()][2];
         int indx = 0;
-        for (Integer id : dict.keySet()) {
-            ArrayList<Integer> scores = dict.get(id);
-            scores.sort((Integer a, Integer b) -> b - a);
+        for (int id : map.keySet()) {
+            PriorityQueue<Integer> q = map.get(id);
             int sum = 0;
-            for (int i = 0; i < 5; i++) {
-                sum += scores.get(i);
+            while (!q.isEmpty()) {
+                sum += q.poll();
             }
             ans[indx][0] = id;
             ans[indx][1] = sum / 5;
             indx++;
         }
+        Arrays.sort(ans, (a, b) -> Integer.compare(a[0], b[0]));
         return ans;
     }
 }
