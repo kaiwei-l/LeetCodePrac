84e8701cbfabb0d59260b01fa5e348e329697cd9
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-02 22:56:38 -0400

    Time: 1 ms (98.66%), Space: 43.9 MB (55.27%) - LeetHub

--- 443-string-compression/443-string-compression.java
+++ 443-string-compression/443-string-compression.java
@@ -1,55 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
-        if (chars.length == 1) return 1;
-        
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-        int n = chars.length;
-        char currChar = chars[left];
-        int updateIndx = 0;
-        while (right < n) {
-            currChar = chars[left];
-            if (chars[left] == chars[right]) {
-                right++;
-            } else {
-                int groupLen = right - left;
-                if (groupLen == 1) {
-                    ans++;
-                    chars[updateIndx] = currChar;
-                    updateIndx++;
-                } else {
-                    int digitCount = String.valueOf(groupLen).length();
-                    String groupLenStr = String.valueOf(groupLen);
-                    ans += 1 + digitCount;
-                    chars[updateIndx] = currChar;
-                    updateIndx++;
-                    for (int i = 0; i < digitCount; i++) {
-                        char currDigit = groupLenStr.charAt(i);
-                        chars[updateIndx] = currDigit;
-                        updateIndx++;
-                    }
+        int endIndx = 0;
+        int indx = 0;
+        while (indx < chars.length) {
+            char currChar = chars[indx];
+            int count = 0;
+            while (indx < chars.length && chars[indx] == currChar) {
+                count++;
+                indx++;
+            }
+            chars[endIndx] = currChar;
+            endIndx++;
+            if (count != 1) {
+                String countStr = String.valueOf(count);
+                for (int i = 0; i < countStr.length(); i++) {
+                    chars[endIndx] = countStr.charAt(i);
+                    endIndx++;
                 }
-                left = right;
             }
         }
-        int groupLen = right - left;
-        if (groupLen == 1) {
-            ans++;
-            chars[updateIndx] = currChar;
-            updateIndx++;
-        } else {
-            int digitCount = String.valueOf(groupLen).length();
-            String groupLenStr = String.valueOf(groupLen);
-            ans += 1 + digitCount;
-            chars[updateIndx] = currChar;
-            updateIndx++;
-            for (int i = 0; i < digitCount; i++) {
-                char currDigit = groupLenStr.charAt(i);
-                chars[updateIndx] = currDigit;
-                updateIndx++;
-            }
-        }
-        return ans;
+        return endIndx;
     }
 }
