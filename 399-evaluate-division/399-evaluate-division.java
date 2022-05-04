d232eab4f541faf02b49057a0207604db341e25a
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-04 17:45:33 -0400

    Time: 4 ms (10.35%), Space: 42.9 MB (37.07%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,31 +1,29 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String, ArrayList<Pair<String, Double>>> adjList = new HashMap<String, ArrayList<Pair<String, Double>>>();
+        HashMap<String, List<Pair<String, Double>>> adjList = new HashMap<>();
         for (int i = 0; i < equations.size(); i++) {
-            List<String> equation = equations.get(i);
-            Double value = values[i];
-            String var1 = equation.get(0);
-            String var2 = equation.get(1);
-            ArrayList<Pair<String, Double>> edges1 = adjList.getOrDefault(var1, new ArrayList<Pair<String, Double>>());
-            ArrayList<Pair<String, Double>> edges2 = adjList.getOrDefault(var2, new ArrayList<Pair<String, Double>>());
-            edges1.add(new Pair(var2, value));
-            edges2.add(new Pair(var1, 1 / value));
-            adjList.put(var1, edges1);
-            adjList.put(var2, edges2);
+            String var1 = equations.get(i).get(0);
+            String var2 = equations.get(i).get(1);
+            Double val = values[i];
+            addToGraph(adjList, var1, var2, val);
+            addToGraph(adjList, var2, var1, 1.0 / val);
         }
         
         double[] ans = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
-            List<String> q = queries.get(i);
-            String var1 = q.get(0);
-            String var2 = q.get(1);
-            if (adjList.containsKey(var1)) {
-                HashMap<String, Boolean> visited = new HashMap<>();
-                visited.put(var1, true);
-                ArrayDeque<String> stack = new ArrayDeque<>();
-                stack.add(var1);
-                Double res = dfs(adjList, stack, visited, var2);
-                ans[i] = res;
+            String var1 = queries.get(i).get(0);
+            String var2 = queries.get(i).get(1);
+            if (adjList.containsKey(var1) && adjList.containsKey(var2)) {
+                if (var1.equals(var2)) {
+                    ans[i] = 1.0;
+                } else {
+                    HashSet<String> visited = new HashSet<>();
+                    visited.add(var1);
+                    ArrayDeque<String> stack = new ArrayDeque<>();
+                    stack.addLast(var1);
+                    Double value = dfs(adjList, visited, stack, var2);
+                    ans[i] = value;
+                }
             } else {
                 ans[i] = -1.0;
             }
@@ -33,26 +31,35 @@ class Solution {
         return ans;
     }
     
-    public double dfs(HashMap<String, ArrayList<Pair<String, Double>>> adjList, ArrayDeque<String> stack, HashMap<String, Boolean> visited, String goal) {
+    private Double dfs(HashMap<String, List<Pair<String, Double>>> adjList, HashSet<String> visited, ArrayDeque<String> stack, String goal) {
         while (!stack.isEmpty()) {
             String node = stack.removeLast();
-            ArrayList<Pair<String, Double>> edges = adjList.get(node);
-            for (Pair<String, Double> p : edges) {
-                String nextNode = p.getKey();
-                if (nextNode.equals(goal)) {
-                    return p.getValue();
-                } else {
-                    if (!visited.containsKey(nextNode) || !visited.get(nextNode)) {
-                        visited.put(nextNode, true);
+            List<Pair<String, Double>> edges = adjList.get(node);
+            for (Pair<String, Double> edge : edges) {
+                String nextNode = edge.getKey();
+                Double weight = edge.getValue();
+                if (!visited.contains(nextNode)) {
+                    if (goal.equals(nextNode)) {
+                        return weight;
+                    } else {
+                        visited.add(nextNode);
                         stack.addLast(nextNode);
-                        double ans = dfs(adjList, stack, visited, goal);
-                        if (ans != -1.0) {
-                            return ans * p.getValue();
-                        }
-                    }   
+                        Double res = dfs(adjList, visited, stack, goal);
+                        if (res != -1.0) return res * weight;
+                    }
                 }
             }
         }
-        return -1;
+        return -1.0;
+    }
+    
+    private void addToGraph(HashMap<String, List<Pair<String, Double>>> adjList, String var1, String var2, Double value) {
+        if (adjList.containsKey(var1)) {
+            adjList.get(var1).add(new Pair(var2, value));
+        } else {
+            List<Pair<String, Double>> list = new ArrayList<>();
+            list.add(new Pair(var2, value));
+            adjList.put(var1, list);
+        }
     }
 }
