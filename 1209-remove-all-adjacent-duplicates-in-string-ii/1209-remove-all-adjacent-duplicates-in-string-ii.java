229fb3c8825ad5ab460ca14730af62f0028d7f6e
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-04 12:19:12 -0400

    Time: 38 ms (68.96%), Space: 58.1 MB (8.19%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,25 +1,35 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
+        Integer limit = k;
         ArrayDeque<Pair<Character, Integer>> stack = new ArrayDeque<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (stack.size() == 0 || stack.getLast().getKey() != s.charAt(i)) {
-                stack.addLast(new Pair<Character, Integer>(s.charAt(i), 1));
+        for (Character c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.addLast(new Pair(c, 1));
             } else {
-                Pair<Character, Integer> p = stack.removeLast();
-                if (p.getValue() + 1 < k) {
-                    stack.addLast(new Pair<Character, Integer>(p.getKey(), p.getValue() + 1));
+                Pair<Character, Integer> top = stack.removeLast();
+                if (c == top.getKey()) {
+                    Integer freq = top.getValue();
+                    stack.addLast(new Pair(c, freq + 1));
+                    while (!stack.isEmpty()) {
+                        Pair p = stack.removeLast();
+                        if ((int)p.getValue() < k) {
+                            stack.addLast(p);
+                            break;
+                        }
+                    }
+                } else {
+                    stack.addLast(top);
+                    stack.addLast(new Pair(c, 1));
                 }
             }
         }
-        
         StringBuilder ans = new StringBuilder();
-        while (!stack.isEmpty()) {
-            Pair<Character, Integer> p = stack.removeLast();
-            for (int i = 0; i < p.getValue(); i++) {
+        for (Pair<Character, Integer> p : stack) {
+            int i = p.getValue();
+            for (int j = 0; j < i; j++) {
                 ans.append(p.getKey());
             }
         }
-        return ans.reverse().toString();
-        
+        return ans.toString();
     }
 }
