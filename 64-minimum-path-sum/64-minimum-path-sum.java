75902b87cc2caedbdd3eccf01f30da645671754b
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-04 23:49:31 -0400

    Time: 4 ms (28.23%), Space: 45.7 MB (61.34%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -3,22 +3,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        int[][] memo = new int[m][n];
-        memo[0][0] = grid[0][0];
-        for (int j = 1; j < n; j++) {
-            memo[0][j] = memo[0][j - 1] + grid[0][j];
-        }
-        for (int i = 1; i < m; i++) {
-            memo[i][0] = memo[i - 1][0] + grid[i][0];
-        }
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                memo[i][j] = Math.min(memo[i - 1][j], memo[i][j - 1]) + grid[i][j];
+        int[] memo = new int[n];
+        memo[0] = grid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j != 0) {
+                    memo[j] = memo[j - 1] + grid[i][j];
+                } else if (j == 0 && i != 0) {
+                    memo[j] = memo[j] + grid[i][j];
+                } else if (i != 0 && j != 0) {
+                    memo[j] = Math.min(memo[j], memo[j - 1]) + grid[i][j];
+                }
             }
         }
-        return memo[m - 1][n - 1];
-        
-        
+        return memo[n - 1];
     }
 }
