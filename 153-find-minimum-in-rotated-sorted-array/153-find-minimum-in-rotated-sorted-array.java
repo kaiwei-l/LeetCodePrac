2c1678136c149947299b62219d59a077ffdae0f7
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-05 14:21:06 -0400

    Time: 1 ms (32.65%), Space: 42.9 MB (25.54%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -13,7 +13,7 @@ class Solution {
             if (nums[mid] < nums[mid - 1]) {
                 return nums[mid];
             }
-            if (nums[mid] > nums[0]) {
+            if (nums[mid] >= nums[0]) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
