cedbbb18c368c09a77a9f73afd9e3ee4a92a56b9
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-04 23:39:19 -0400

    Time: 2 ms (87.47%), Space: 45 MB (80.74%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -2,36 +2,23 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
-        dp[0][0] = grid[0][0];
-        visited[0][0] = true;
-        // for (int i = 1; i < m; i++) {
-        //     dp[i][0] = grid[i][0] + dp[i - 1][0];
-        //     visited[i][0] = true;
-        // }
-        // for (int j = 1; j < n; j++) {
-        //     dp[0][j] = grid[0][j] + dp[0][j - 1];
-        //     visited[0][j] = true;
-        // }
-        return backtrack(dp, visited, grid, m - 1, n - 1);
-    }
-    
-    private int backtrack(int[][] dp, boolean[][] visited, int[][] grid, int row, int col) {
-        if (visited[row][col]) {
-            return dp[row][col];
-        } else {
-            int newRow = row - 1;
-            int newCol = col - 1;
-            visited[row][col] = true;
-            if (newRow >= 0 && newCol >= 0) {
-                dp[row][col] = Math.min(backtrack(dp, visited, grid, row, newCol), backtrack(dp, visited, grid, newRow, col)) + grid[row][col];   
-            } else if (newRow < 0) {
-                dp[row][col] = backtrack(dp, visited, grid, row, newCol) + grid[row][col];
-            } else {
-                dp[row][col] = backtrack(dp, visited, grid, newRow, col) + grid[row][col];
-            }
-            return dp[row][col];
+        
+        int[][] memo = new int[m][n];
+        memo[0][0] = grid[0][0];
+        for (int j = 1; j < n; j++) {
+            memo[0][j] = memo[0][j - 1] + grid[0][j];
         }
+        for (int i = 1; i < m; i++) {
+            memo[i][0] = memo[i - 1][0] + grid[i][0];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                memo[i][j] = Math.min(memo[i - 1][j], memo[i][j - 1]) + grid[i][j];
+            }
+        }
+        return memo[m - 1][n - 1];
+        
+        
     }
 }
