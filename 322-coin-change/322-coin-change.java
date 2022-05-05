dc0845f821c614cee827e45e9c538bce929799ca
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-05 14:54:44 -0400

    Time: 33 ms (37.82%), Space: 44.5 MB (71.04%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,31 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        int n = coins.length;
-        int[][] dp = new int[amount + 1][n + 1];
-        
-        for (int i = 0; i < n + 1; i++) dp[0][i] = 0;
-        for (int value = 1; value <= amount; value++) dp[value][0] = -1;
-        for (int value = 1; value <= amount; value++) {
-            for (int i = 1; i < n + 1; i++) {
-                int coin = coins[i - 1];
-                if (coin > value) {
-                    dp[value][i] = dp[value][i - 1];
-                } else if (coin == value) {
-                    dp[value][i] = 1;
-                } else {
-                    if (dp[value][i - 1] != -1 && dp[value - coin][i] != -1) {
-                        dp[value][i] = Math.min(dp[value - coin][i] + 1, dp[value][i - 1]);
-                    } else if (dp[value][i - 1] != -1) {
-                        dp[value][i] = dp[value][i - 1];
-                    } else if (dp[value - coin][i] != -1){
-                       dp[value][i] = dp[value - coin][i] + 1; 
-                    } else {
-                        dp[value][i] = -1;
-                    }
+        int[] memo = new int[amount + 1];
+        Arrays.fill(memo, amount + 1);
+        memo[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    memo[i] = Math.min(memo[i], memo[i - coin] + 1);
                 }
             }
         }
-        return dp[amount][n];
+        return (memo[amount] == (amount + 1)) ? -1 : memo[amount];
     }
 }
