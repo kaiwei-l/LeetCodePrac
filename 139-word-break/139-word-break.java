41cbf305a03a0a00c0a3eee53b0b487161ad8073
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-16 16:38:12 -0400

    Time: 7 ms (67.12%), Space: 42.3 MB (83.03%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,44 +1,23 @@
-class Trie {
-    HashMap<Character, Trie> children;
-    String word;
-    
-    public Trie() {
-        this.children = new HashMap<>();
-        this.word = null;
-    }
-}
-
 class Solution {
-    public void insert(String word, Trie node) {
-        for (int i = 0; i < word.length(); i++) {
-            Character currChar = word.charAt(i);
-            if (!node.children.containsKey(currChar)) {
-                Trie nextNode = new Trie();
-                node.children.put(currChar, nextNode);
-            }
-            node = node.children.get(currChar);
-        }
-        node.word = word;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Boolean[] memo = new Boolean[s.length()];
+        return dp(s, 0, memo, wordDict);
     }
     
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Trie root = new Trie();
-        for (int i = 0; i < wordDict.size(); i++) {
-            String currWord = wordDict.get(i);
-            insert(currWord, root);
+    public boolean dp(String s, int start, Boolean[] memo, List<String> wordDict) {
+        if (start == s.length()) {
+            return true;
         }
-        
-        boolean[] wordBreak = new boolean[s.length() + 1];
-        wordBreak[0] = true;
-        for (int left = 0; left < s.length(); left++) {
-            Trie node = root;
-            for (int right = left; right < s.length(); right++) {
-                Character currChar = s.charAt(right);
-                if (!node.children.containsKey(currChar)) break;
-                if (wordBreak[left] && node.children.get(currChar).word != null) wordBreak[right + 1] = true;
-                node = node.children.get(currChar);
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && dp(s, end, memo, wordDict)) {
+                memo[start] = true;
+                return memo[start];
             }
         }
-        return wordBreak[wordBreak.length - 1];
+        memo[start] = false;
+        return memo[start];
     }
 }
