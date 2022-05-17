0cd98145dfdc7f624c3275870c19aa15c2356c67
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-16 22:30:48 -0400

    Time: 2 ms (61.42%), Space: 42.8 MB (24.72%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,31 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
         int[] memo = new int[s.length() + 1];
-        Arrays.fill(memo, -1);
-        return dp(s, 0, memo);
-    }
-    
-    public int dp(String s, int indx, int[] memo) {
-        if (memo[indx] != -1) {
-            return memo[indx];
+        memo[0] = 1;
+        memo[1] = ((s.charAt(0) == '0') ? 0 : 1);
+        for (int i = 1; i < s.length(); i++) {
+            int indx = i + 1;
+            if (s.charAt(i) != '0') {
+                memo[indx] += memo[indx - 1];
+            }
+            int twoDigit = Integer.valueOf(s.substring(i - 1, i + 1));
+            if (twoDigit <= 26 && twoDigit >= 10) {
+                memo[indx] += memo[indx - 2];
+            }
         }
-        
-        if (indx == s.length()) {
-            return 1;
-        }
-        
-        if (s.charAt(indx) == '0') {
-            return 0;
-        }
-        
-        if (indx == s.length() - 1) {
-            return 1;
-        }
-        int res = dp(s, indx + 1, memo);
-        if (Integer.valueOf(s.substring(indx, indx + 2)) <= 26) {
-            res += dp(s, indx + 2, memo);
-        }
-        memo[indx] = res;
-        return res;
+        return memo[s.length()];
     }
 }
