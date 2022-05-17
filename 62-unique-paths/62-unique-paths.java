e40cca1ae5adc369e7dd57b9fd42c5060c19965b
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-16 23:27:18 -0400

    Time: 1 ms (40.99%), Space: 40 MB (72.83%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,7 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[] memo = new int[n];
-        memo[0] = 1;
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 if (row == 0 && col != 0) {
