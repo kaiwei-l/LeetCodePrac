facc7dc075018b71e194fe9c7d4811d0925b66bb
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-16 23:27:10 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (24.09%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,20 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] memo = new int[m][n];
-        memo[0][0] = 1;
+        int[] memo = new int[n];
+        memo[0] = 1;
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 if (row == 0 && col != 0) {
-                    memo[row][col] = memo[row][col - 1];
+                    memo[col] = memo[col - 1];
                 } else if (col == 0 && row != 0) {
-                    memo[row][col] = memo[row - 1][col];
+                    memo[col] = memo[col];
                 } else if (row == 0 && col == 0) {
-                    memo[row][col] = 1;
+                    memo[col] = 1;
                 } else {
-                    memo[row][col] = memo[row - 1][col] + memo[row][col - 1];
+                    memo[col] += memo[col - 1];
                 }
             }
         }
-        return memo[m - 1][n - 1];
+        return memo[n - 1];
     }
 }
