ec0b3ab30c779a6d35addc92401e1d1c79a65b37
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-25 22:40:42 -0400

    Time: 1635 ms (5.01%), Space: 117.7 MB (12.72%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,57 +1,41 @@
 class Solution {
-    public boolean canVisit(int row, int col, int rowLimit, int colLimit) {
-        if (row >= 0 && row < rowLimit && col >= 0 && col < colLimit) return true;
-        return false;
-    }
-    
-    public boolean dfs(ArrayDeque<Pair<Integer, Integer>> stack, int rowLimit, int colLimit, char[][] board, String word, int wordIndx) {
-        wordIndx++;
-        while (!stack.isEmpty() && wordIndx < word.length()) {
-            Pair<Integer, Integer> cell = stack.removeLast();
-            int row = cell.getKey();
-            int col = cell.getValue();
-            char currC = board[row][col];
-            board[row][col] = '1';
-            for (int rowChange = -1; rowChange < 2; rowChange++) {
-                int newRow = row + rowChange;
-                if (canVisit(newRow, col, rowLimit, colLimit) && word.charAt(wordIndx) == board[newRow][col] && board[newRow][col] != '1') {
-                    stack.add(new Pair(newRow, col));
-                    boolean res = dfs(stack, rowLimit, colLimit, board, word, wordIndx);
-                    if (res) {
-                        board[row][col] = currC;
-                        return true;
-                    }
-                }
-            }
-            for (int colChange = -1; colChange < 2; colChange++) {
-                int newCol = col + colChange;
-                if (canVisit(row, newCol, rowLimit, colLimit) && word.charAt(wordIndx) == board[row][newCol] && board[row][newCol] != '1') {
-                    stack.add(new Pair(row, newCol));
-                    boolean res = dfs(stack, rowLimit, colLimit, board, word, wordIndx);
-                    if (res) {
-                        board[row][col] = currC;
-                        return true;
-                    }
-                }
-            }
-            board[row][col] = currC;
-        }
-        if (wordIndx == word.length()) return true;
-        return false;
-    }
+    int m;
+    int n;
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     
     public boolean exist(char[][] board, String word) {
-        int rowLimit = board.length;
-        int colLimit = board[0].length;
-        ArrayDeque<Pair<Integer, Integer>> stack = new ArrayDeque<>();
-        int wordIndx = 0;
-        for (int i = 0; i < rowLimit; i++) {
-            for (int j = 0; j < colLimit; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    stack.add(new Pair(i, j));
-                    boolean res = dfs(stack, rowLimit, colLimit, board, word, wordIndx);
+        HashSet<Pair<Integer, Integer>> visited = new HashSet<>();
+        int indx = 0;
+        m = board.length;
+        n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word.charAt(indx) == board[i][j]) {
+                    Pair<Integer, Integer> point = new Pair(i, j);
+                    visited.add(point);
+                    boolean res = dfs(board, word, visited, point, indx + 1);
                     if (res) return true;
+                    indx = 0;
+                    visited.clear();
                 }
+            }
+        }
+        return false;
+    }
+    
+    public boolean dfs(char[][] board, String word, HashSet<Pair<Integer, Integer>> visited, Pair<Integer, Integer> point, int indx) {
+        if (indx == word.length()) return true;
+        int row = point.getKey();
+        int col = point.getValue();
+        for (int[] dir : directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            Pair<Integer, Integer> newPoint = new Pair(newRow, newCol);
+            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited.contains(newPoint) && board[newRow][newCol] == word.charAt(indx)) {
+                visited.add(newPoint);
+                boolean res = dfs(board, word, visited, newPoint, indx + 1);
+                if (res) return res;
+                visited.remove(newPoint);
             }
         }
         return false;
