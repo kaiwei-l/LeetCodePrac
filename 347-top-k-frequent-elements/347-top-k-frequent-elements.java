7e968e5e0114a234d5a2fbe5b32899a2362bed27
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-29 23:42:35 -0400

    Time: 14 ms (69.55%), Space: 50.6 MB (43.32%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,18 +1,56 @@
 class Solution {
+    HashMap<Integer, Integer> freq;
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        PriorityQueue<Pair<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b.getValue(), a.getValue()));
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        if (k == nums.length) return nums;
+        freq = new HashMap<>();
+        for (int n : nums) {
+            freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
-        for (Integer num : freq.keySet()) {
-            maxHeap.add(new Pair(num, freq.get(num)));
+        
+        int[] unique = new int[freq.size()];
+        int i = 0;
+        for (int n : freq.keySet()) {
+            unique[i] = n;
+            i++;
         }
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            Pair<Integer, Integer> p = maxHeap.poll();
-            ans[i] = p.getKey();
+        
+        int n = unique.length;
+        int target = n - k;
+        int pivot = quickselect(unique, 0, n - 1, target);
+        return Arrays.copyOfRange(unique, pivot, n);
+    }
+    
+    public int quickselect(int[] unique, int left, int right, int target) {
+        while (left <= right) {
+            int pivot = partition(unique, left, right, target);
+            if (pivot == target) {
+                return pivot;
+            } else if (pivot < target) {
+                return quickselect(unique, pivot + 1, right, target);
+            } else {
+                return quickselect(unique, left, pivot - 1, target);
+            }
         }
-        return ans;
+        return -1;
+    }
+    
+    public int partition(int[] unique, int left, int right, int target) {
+        int pivotNum = unique[right];
+        int pivot = left - 1;
+        for (int i = left; i < right; i++) {
+            if (freq.get(unique[i]) < freq.get(pivotNum)) {
+                pivot++;
+                swap(unique, pivot, i);
+            }
+        }
+        pivot++;
+        swap(unique, pivot, right);
+        return pivot;
+    }
+    
+    public void swap(int[] unique, int left, int right) {
+        int temp = unique[left];
+        unique[left] = unique[right];
+        unique[right] = temp;
     }
 }
