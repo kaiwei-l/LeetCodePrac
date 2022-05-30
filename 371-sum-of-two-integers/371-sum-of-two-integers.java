467c007272dde620d0aa7327b03d3a407210ea3d
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-30 17:37:13 -0400

    Time: 0 ms (100.00%), Space: 39.2 MB (77.47%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -1,5 +1,26 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a + b;
+        int x = Math.abs(a);
+        int y = Math.abs(b);
+        if (x < y) return getSum(b, a);
+        
+        int sign = (a > 0) ? 1 : -1;
+        
+        if (a * b >= 0) {
+            while (y != 0) {
+                int ans = x ^ y;
+                int carry = (x & y) << 1;
+                x = ans;
+                y = carry;
+            }
+        } else {
+            while (y != 0) {
+                int ans = x ^ y;
+                int borrow = ((~x) & y) << 1;
+                x = ans;
+                y = borrow;
+            }
+        }
+        return x * sign;
     }
 }
