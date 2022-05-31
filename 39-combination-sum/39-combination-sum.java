40bb654507f94c52018e0fba13bb59905358e984
Kaiwei L <kaiweiluo5@gmail.com>
2022-05-31 01:12:57 -0400

    Time: 4 ms (78.32%), Space: 42.7 MB (86.71%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,24 +1,20 @@
 class Solution {
-    List<List<Integer>> ans;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        ans = new ArrayList<>();
-        combinationAux(candidates, target, new ArrayList<Integer>(), 0);
-        return ans;
-    }
-    
-    public void combinationAux(int[] candidates, int target, ArrayList<Integer> curr, int indx) {
-        if (target < 0) return;
-        
-        if (target == 0) {
-            List<Integer> res = new ArrayList<>(curr);
-            ans.add(res);
-            return;
+        List<List<List<Integer>>> memo = new ArrayList<>();
+        for (int i = 0; i <= target; i++) {
+            memo.add(new ArrayList<>());
         }
-        
-        for (int i = indx; i < candidates.length; i++) {
-            curr.add(candidates[i]);
-            combinationAux(candidates, target - candidates[i], curr, i);
-            curr.remove(curr.size() - 1);
+        memo.get(0).add(new ArrayList<>());
+        for (int c : candidates) {
+            for (int i = c; i <= target; i++) {
+                List<List<Integer>> temp = memo.get(i - c);
+                for (List<Integer> combo : temp) {
+                    List<Integer> newCombo = new ArrayList<>(combo);
+                    newCombo.add(c);
+                    memo.get(i).add(newCombo);
+                }
+            }
         }
+        return memo.get(target);
     }
 }
