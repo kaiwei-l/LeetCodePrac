a21cff35acbfcdd77339dde8ac992ebcc577a0bf
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-07 15:50:09 -0400

    Time: 5 ms (91.37%), Space: 42.1 MB (98.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,27 +1,24 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) return 0;
-        int maxLen = Integer.MIN_VALUE;
-        int currLen = 0;
+        HashMap<Character, Integer> lastIndx = new HashMap<>();
         int left = 0;
-        HashMap<Character, Integer> freq = new HashMap<>();
+        int ans = 0;
         for (int right = 0; right < s.length(); right++) {
             Character c = s.charAt(right);
-            if (!freq.containsKey(c) || freq.get(c) == 0) {
-                freq.put(c, freq.getOrDefault(c, 0) + 1);
-                currLen++;
-                maxLen = Math.max(maxLen, currLen);
-            } else {
-                freq.put(c, freq.get(c) + 1);
-                currLen++;
-                while (freq.get(c) > 1 && left <= right) {
-                    Character leftC = s.charAt(left);
-                    freq.put(leftC, freq.get(leftC) - 1);
+            if (lastIndx.containsKey(c)) {
+                if (left <= lastIndx.get(c)) {
+                    left = lastIndx.get(c);
                     left++;
-                    currLen--;
+                    ans = Math.max(ans, right - left + 1);
+                } else {
+                    ans = Math.max(ans, right - left + 1);
                 }
+                lastIndx.put(c, right);
+            } else {
+                ans = Math.max(ans, right - left + 1);
+                lastIndx.put(c, right);
             }
         }
-        return maxLen;
+        return ans;
     }
 }
