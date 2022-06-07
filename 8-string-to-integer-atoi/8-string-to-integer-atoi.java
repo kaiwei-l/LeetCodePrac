23790b8c45d13b7e796b2d9366c170d255c1f24e
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-07 11:42:51 -0400

    Time: 4 ms (33.91%), Space: 42.7 MB (64.54%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,36 +1,34 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0;
-        StringBuilder num = new StringBuilder();
+        int indx = 0;
+        if (s == null || s.length() == 0) return 0;
+        while (indx < s.length() && s.charAt(indx) == ' ') {
+            indx++;
+        }
+        
         int sign = 1;
-        int res = 0;
-        
-        while (i < s.length() && s.charAt(i) == ' ') {
-            i++;
+        if (indx < s.length() && s.charAt(indx) == '-') {
+            sign = -1;
+            indx++;
+        } else if (indx < s.length() && s.charAt(indx) == '+') {
+            indx++;
         }
         
-        if (i < s.length() && s.charAt(i) == '-') {
-            sign *= -1;
-            i++;
-        } else if (i < s.length() && s.charAt(i) == '+') {
-            sign *= 1;
-            i++;
-        }
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
-        while (i < s.length() && Character.isDigit(s.charAt(i))) {
-            int digit = Character.getNumericValue(s.charAt(i));
-            if (res > max / 10 || (res == max / 10 && digit > max % 10)) {
-                if (sign == 1) {
-                    return max;
-                } else {
-                    return min;
-                }
+        int ans = 0;
+        
+        while (indx < s.length() && Character.isDigit(s.charAt(indx))) {
+            char c = s.charAt(indx);
+            int d = c - '0';
+            if (ans == Integer.MAX_VALUE / 10 && d <= Integer.MAX_VALUE % 10) {
+                ans = ans * 10 + d;
+            } else if (ans >= Integer.MAX_VALUE / 10) {
+                ans = Integer.MAX_VALUE;
+                return (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            } else {
+                ans = ans * 10 + d;
             }
-            res = res * 10 + digit;
-            i++;
+            indx++;
         }
-        return res * sign;
-        
+        return (sign == 1) ? ans : -1 * ans;
     }
 }
