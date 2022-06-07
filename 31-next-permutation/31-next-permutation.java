c0b2f807a03173f4cd8d637ff363c79dbd0c0679
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-07 16:27:41 -0400

    Time: 1 ms (82.74%), Space: 43.5 MB (59.60%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,29 +1,29 @@
 class Solution {
-    public void exchange(int left, int right, int[] nums) {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        for (int indx = n - 1; indx > 0; indx--) {
+            if (nums[indx] > nums[indx - 1]) {
+                int rightMin = n - 1;
+                while (nums[rightMin] <= nums[indx - 1]) {
+                    rightMin--;
+                }
+                swap(nums, rightMin, indx - 1);
+                reverse(nums, indx, n - 1);
+                return;
+            }
+        }
+        reverse(nums, 0, n - 1);
+    }
+    
+    public void swap(int[] nums, int left, int right) {
         int temp = nums[left];
         nums[left] = nums[right];
         nums[right] = temp;
     }
     
-    public void nextPermutation(int[] nums) {
-        int midPoint = -1;
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                int swapPoint = i;
-                for (int indx = i; indx < nums.length; indx++) {
-                    if (nums[i - 1] < nums[indx]) {
-                        swapPoint = indx;
-                    }
-                }
-                exchange(i - 1, swapPoint, nums);
-                midPoint = i;
-                break;
-            }
-        }
-        int left = (midPoint == -1) ? 0 : midPoint;
-        int right = nums.length - 1;
-        while (left < right) {
-            exchange(left, right, nums);
+    public void reverse(int[] nums, int left, int right) {
+        while (left <= right) {
+            swap(nums, left, right);
             left++;
             right--;
         }
