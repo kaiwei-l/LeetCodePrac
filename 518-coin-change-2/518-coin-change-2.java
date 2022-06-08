610c91ed089cb20b6c729e597ddf30d596a59d8a
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-07 23:29:17 -0400

    Time: 4 ms (78.56%), Space: 41.8 MB (66.09%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -2,11 +2,9 @@ class Solution {
     public int change(int amount, int[] coins) {
         int[] memo = new int[amount + 1];
         memo[0] = 1;
-        for (int coin : coins) {
+        for (int c : coins) {
             for (int i = 1; i <= amount; i++) {
-                if (coin <= i) {
-                    memo[i] += memo[i - coin];
-                }
+                if (i >= c) memo[i] += memo[i - c];
             }
         }
         return memo[amount];
