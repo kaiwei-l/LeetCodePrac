ee19afd2a20eb8af4a35a028fc4a09135ceeb24e
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-08 00:58:51 -0400

    Time: 136 ms (30.57%), Space: 41.4 MB (63.38%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,34 +1,33 @@
 class Solution {
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     int m;
     int n;
-    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    int ans = 0;
     
     public int getMaximumGold(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
+        int ans = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] != 0) {
-                    backtrack(i, j, grid, 0);
-                }
+                int gold = backtrack(grid, i, j);
+                ans = Math.max(ans, gold);
             }
         }
         return ans;
     }
     
-    public void backtrack(int row, int col, int[][] grid, int res) {
+    public int backtrack(int[][] grid, int row, int col) {
         int gold = grid[row][col];
+        int goldFromNext = 0;
         grid[row][col] = 0;
-        res += gold;
         for (int[] dir : directions) {
             int newRow = row + dir[0];
             int newCol = col + dir[1];
             if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] != 0) {
-                backtrack(newRow, newCol, grid, res);
+                goldFromNext = Math.max(goldFromNext, backtrack(grid, newRow, newCol));
             }
         }
-        ans = Math.max(ans, res);
         grid[row][col] = gold;
+        return gold + goldFromNext;
     }
 }
