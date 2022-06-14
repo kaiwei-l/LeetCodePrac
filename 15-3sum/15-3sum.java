b002d80918b7c2746fabf70f2731ed39596d594c
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-14 19:49:48 -0400

    Time: 27 ms (76.77%), Space: 60.1 MB (28.23%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,35 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        int n = nums.length;
-        if (n < 3) return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length < 3) return ans;
         Arrays.sort(nums);
-        
-        int indx = 0;
-        while (indx < n - 2) {
-            int complement = -1 * nums[indx];
-            int left = indx + 1;
-            int right = n - 1;
-            while (left < right) {
-                if (nums[left] + nums[right] == complement) {
-                    List<Integer> res = new ArrayList<Integer>();
-                    res.add(nums[indx]);
-                    res.add(nums[left]);
-                    res.add(nums[right]);
-                    ans.add(res);
-                    left++;
-                    while (left < right && nums[left] == nums[left - 1]) {
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int complement = -1 * nums[i];
+                int left = i + 1;
+                int right = n - 1;
+                while (left < right) {
+                    if (nums[left] + nums[right] == complement) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[left]);
+                        temp.add(nums[right]);
+                        ans.add(temp);
+                        left++;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                    } else if (nums[left] + nums[right] > complement) {
+                        right--;
+                    } else {
                         left++;
                     }
-                } else if (nums[left] + nums[right] < complement) {
-                    left++;
-                } else {
-                    right--;
                 }
-            }
-            indx++;
-            while (indx < n - 2 && nums[indx] == nums[indx - 1]) {
-                indx++;
             }
         }
         return ans;
