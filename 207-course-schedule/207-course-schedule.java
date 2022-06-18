7af0349c2aa4d131fcc3a1b67fc5cf55b9737709
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-17 22:49:58 -0400

    Time: 11 ms (39.72%), Space: 49.6 MB (24.21%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,39 +1,34 @@
 class Solution {
-    boolean hasCycle = false;
-    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         HashMap<Integer, List<Integer>> adjList = new HashMap<>();
-        HashMap<Integer, Integer> color = new HashMap<>();
+        HashMap<Integer, Integer> indegree = new HashMap<>();
         for (int i = 0; i < numCourses; i++) {
             adjList.put(i, new ArrayList<Integer>());
-            color.put(i, 1);
+            indegree.put(i, 0);
         }
-        for (int i = 0; i < prerequisites.length; i++) {
-            int src = prerequisites[i][1];
-            int dest = prerequisites[i][0];
+        for (int[] edge : prerequisites) {
+            int src = edge[1];
+            int dest = edge[0];
             adjList.get(src).add(dest);
+            indegree.put(dest, indegree.get(dest) + 1);
         }
-        ArrayDeque<Integer> ans = new ArrayDeque<Integer>();
-        for (int i = 0; i < numCourses; i++) {
-            if (color.get(i) == 1) {
-                dfs(adjList, color, ans, i);
-                if (hasCycle) return false;
+        
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for (Integer node : indegree.keySet()) {
+            if (indegree.get(node) == 0) q.addLast(node);
+        }
+        
+        while (!q.isEmpty()) {
+            Integer node = q.removeFirst();
+            for (Integer next : adjList.get(node)) {
+                indegree.put(next, indegree.get(next) - 1);
+                if (indegree.get(next) == 0) q.addLast(next);
             }
+        }
+        
+        for (Integer node : indegree.keySet()) {
+            if (indegree.get(node) != 0) return false;
         }
         return true;
-    }
-    
-    public void dfs(HashMap<Integer, List<Integer>> adjList, HashMap<Integer, Integer> color, ArrayDeque<Integer> ans, Integer node) {
-        color.put(node, 0);
-        for (Integer next : adjList.get(node)) {
-            if (color.get(next) == 1) {
-                color.put(next, 0);
-                dfs(adjList, color, ans, next);
-            } else if (color.get(next) == 0) {
-                hasCycle = true;
-            }
-        }
-        color.put(node, -1);
-        ans.addFirst(node);
     }
 }
