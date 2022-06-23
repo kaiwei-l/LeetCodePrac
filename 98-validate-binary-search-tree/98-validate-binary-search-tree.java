ad7decb7978403b184b8bcf8513aa3907b571389
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-23 14:51:14 -0400

    Time: 1 ms (58.33%), Space: 44.1 MB (34.90%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,13 +15,13 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return bstAux(root, null, null);
+        return check(root, null, null);
     }
     
-    public boolean bstAux(TreeNode node, Integer lo, Integer hi) {
+    public boolean check(TreeNode node, Integer min, Integer max) {
         if (node == null) return true;
-        if (lo != null && lo >= node.val) return false;
-        if (hi != null && hi <= node.val) return false;
-        return bstAux(node.left, lo, node.val) && bstAux(node.right, node.val, hi);
+        if (min != null && node.val <= min) return false;
+        if (max != null && node.val >= max) return false;
+        return check(node.left, min, node.val) && check(node.right, node.val, max);
     }
 }
