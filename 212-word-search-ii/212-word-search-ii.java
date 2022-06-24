c2a0a5a7ad767af41de5f14e373afadd1bdb1638
Kaiwei L <kaiweiluo5@gmail.com>
2022-06-24 01:16:50 -0400

    Time: 671 ms (29.93%), Space: 44 MB (71.61%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.java
+++ 212-word-search-ii/212-word-search-ii.java
@@ -1,72 +1,61 @@
 class Trie {
-    HashMap<Character, Trie> map;
     String word;
+    HashMap<Character, Trie> map;
     
     public Trie() {
-        this.map = new HashMap<>();
-        this.word = null;
+        word = null;
+        map = new HashMap<>();
     }
 }
+
 class Solution {
-    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     Trie root;
+    List<String> ans;
     int m;
     int n;
-    HashSet<Pair<Integer, Integer>> visited;
-    List<String> ans;
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     
     public List<String> findWords(char[][] board, String[] words) {
-        ans = new ArrayList<>();
         root = new Trie();
-        m = board.length;
-        n = board[0].length;
-        
+        ans = new ArrayList<>();
         for (String w : words) {
             insert(root, w);
         }
-        
+        m = board.length;
+        n = board[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (root.map.containsKey(board[i][j])) {
-                    visited = new HashSet<>();
-                    visited.add(new Pair(i, j));
-                    search(new Pair(i, j), root, board);
-                }
+                if (root.map.containsKey(board[i][j])) search(board, i, j, root.map.get(board[i][j]));
             }
         }
         return ans;
     }
     
-    public void search(Pair<Integer, Integer> point, Trie parent, char[][] board) {
-        int row = point.getKey();
-        int col = point.getValue();
-        char c = board[row][col];
-        Trie node = parent.map.get(c);
-        if (node.word != null) {
-            ans.add(node.word);
-            node.word = null;
+    public void search(char[][] board, int row, int col, Trie root) {
+        if (root.word != null) {
+            ans.add(root.word);
+            root.word = null;
         }
+        
+        Character c = board[row][col];
+        board[row][col] = '1';
         for (int[] dir : directions) {
             int newRow = row + dir[0];
             int newCol = col + dir[1];
-            Pair<Integer, Integer> next = new Pair(newRow, newCol);
-            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited.contains(next) && node.map.containsKey(board[newRow][newCol])) {
-                visited.add(next);
-                search(next, node, board);
+            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && root.map.containsKey(board[newRow][newCol])) {
+                search(board, newRow, newCol, root.map.get(board[newRow][newCol]));
             }
         }
-        visited.remove(point);
-        if (node.map.size() == 0) parent.map.remove(c);
+        board[row][col] = c;
     }
     
-    public void insert(Trie root, String word) {
-        Trie node = root;
-        for (Character c : word.toCharArray()) {
-            if (!node.map.containsKey(c)) {
-                node.map.put(c, new Trie());
+    public void insert(Trie root, String w) {
+        for (Character c : w.toCharArray()) {
+            if (!root.map.containsKey(c)) {
+                root.map.put(c, new Trie());
             }
-            node = node.map.get(c);
+            root = root.map.get(c);
         }
-        node.word = word;
+        root.word = w;
     }
 }
